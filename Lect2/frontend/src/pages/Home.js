2f4019ce98ae94cf707178364ec3914227bb98a9
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2025-06-05 00:56:06 -0700

    Added workout details

--- Lect2/frontend/src/pages/Home.js
+++ Lect2/frontend/src/pages/Home.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+
+// Components
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () =>{
+
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchworkouts = async() =>{
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok){
+                setWorkouts(json)
+            }
+        }
+        fetchworkouts()
+    }, [])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <div key={workout._id}>
+                        <WorkoutDetails workout={workout} />
+                        <hr/>
+                    </div>
+                ))}
+                
+            </div>
 
         </div>
     )
